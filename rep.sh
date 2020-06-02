17fe27aec6788ad5b0514dc2de8a88d2a805f739
Xiangruili-seed <66310256+Xiangruili-seed@users.noreply.github.com>
2020-06-02 20:43:30 +0800

    change

--- rep.sh
+++ rep.sh
@@ -13,8 +13,9 @@ wget ${url2}
 samtools sort -@ 6 -m 4G -O bam -o ${sample}.sorted.bam ${sample}.bam
 samtools sort -@ 6 -m 4G -O bam -o ${sample2}.sorted.bam ${sample2}.bam
 samtools merge -@ 6 -c sorted.merged.bam *.sorted.bam
-samtools view -c sorted.merged.bam >reads.txt
 bamToBed -i sorted.merged.bam|awk '$1!~/_/{print $0}'|sort -k1,1 -k2,2n > sorted.merged.bed
+wc -l sorted.merged.bed >reads.txt
+
 cat sorted.merged.bed| awk '{print ($3-$2)+1"\t"}'>${tissue}.read_length.txt
 bedtools closest -a  sorted.merged.bed -b /data/tusers/lixiangr/lncRNA/mouse/ref/sorted_gene.bed -d -t "first" |awk '{print $11}' >${tissue}.read_distance.txt
 bedtools closest -a  sorted.merged.bed -b /data/tusers/lixiangr/lncRNA/mouse/ref/sorted_gene.bed -d  >${tissue}.close_all.txt
--- un.sh
+++ un.sh
@@ -7,8 +7,8 @@ mkdir -p ${data}
 cd ${data}
 wget ${url}
 samtools sort -@ 6 -m 4G -O bam -o ${sample}.sorted.bam ${sample}.bam
-samtools view -c ${sample}.sorted.bam >reads.txt
 bamToBed -i ${sample}.sorted.bam|awk '$1!~/_/{print $0}'|sort -k1,1 -k2,2n  > sorted.merged.bed
+wc -l sorted.merged.bed >reads.txt
 cat sorted.merged.bed| awk '{print ($3-$2)+1}'>${tissue}.read_length.txt
 bedtools closest -a  sorted.merged.bed -b /data/tusers/lixiangr/lncRNA/mouse/ref/sorted_gene.bed -d -t "first" |awk '{print $11}' >${tissue}.read_distance.txt
 bedtools closest -a  sorted.merged.bed -b /data/tusers/lixiangr/lncRNA/mouse/ref/sorted_gene.bed -d -t "first" >${tissue}.close.txt
