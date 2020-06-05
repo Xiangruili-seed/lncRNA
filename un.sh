a6773c1a127045d57f247351607de8baf056d724
Xiangruili-seed <66310256+Xiangruili-seed@users.noreply.github.com>
2020-06-05 17:14:19 +0800

    c

--- un.sh
+++ un.sh
@@ -12,7 +12,7 @@ wc -l sorted.bed>reads.txt
 cat sorted.bed| awk '{print ($3-$2)+1}'>${tissue}.read_length.txt
 cat sorted.bed|awk '{if ($6=="+") print $1"\t"$2"\t"$2+1"\t.\t.\t"$6; else if ($6=="-") print $1"\t"$3-1"\t"$3"\t.\t.\t"$6}'|sort -k1,1 -k2,2n -u >sorted.merged.bed
 
-bedtools closest -a  sorted.merged.bed -b /data/tusers/lixiangr/lncRNA/mouse/ref/sorted_gene.bed -d -s |awk '{print $9}' >${tissue}.read_distance.txt
+bedtools closest -a  sorted.merged.bed -b /data/tusers/lixiangr/lncRNA/mouse/ref/sorted_gene.bed -d -s |awk '{print $13}' >${tissue}.read_distance.txt
 bedtools closest -a  sorted.merged.bed -b /data/tusers/lixiangr/lncRNA/mouse/ref/sorted_gene.bed -d  -s >${tissue}.close_all.txt
 
 
